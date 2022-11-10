f9b73304c9654cc9a5fc5e2c933f6bcb4750014c
Luciana <116033232+lluizacamargo@users.noreply.github.com>
2022-11-10 10:47:27 -0300

    Desafio_Area_JS

---
+++ Level 1/Desafio_Area.js
@@ -0,0 +1,16 @@
+let raio = 10;
+const PI = 3.141592;
+let areaCirc
+
+areaCirc = PI*raio*raio;
+
+console.log("O valor da área é: " + areaCirc + " m²")
+
+// console.log("O valor é: " + PI*raio*raio);  
+
+
+
+
+
+
+
